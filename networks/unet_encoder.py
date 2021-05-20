d2db5dfb9c06fa7b20eedcd96ab31f351727d573
Zeju1997 <47625089+Zeju1997@users.noreply.github.com>
2021-05-20 12:11:55 +0200

    Add files via upload

---
+++ networks/unet_encoder.py
@@ -0,0 +1,25 @@
+import torch.nn.functional as F
+
+from .unet_parts import *
+
+class UNet_encoder(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(UNet_encoder, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        z = self.down4(x4)
+        return z
